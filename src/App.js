a2c97e616dd928daaa9699360df67c769e782936
loonatiq <86408147+loonatiq@users.noreply.github.com>
2021-12-28 21:50:16 +0900

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { useState } from 'react';
+import Button from './Button';
 import Dice from './Dice';
 
+function Random(n){
+  return Math.ceil(Math.random() * n);
+}
+
 function App() {
+  const [num, setNum] = useState(1);
+
+  const handleRollClick = () => {
+    const nextNum = Random(6);
+    setNum(nextNum);
+    console.log(nextNum);
+  };
+
+  const handleClearClick = () => {
+    setNum(1);
+    console.log('처음부터');
+  };
+
   return (
-    <div>
-      <Dice color="blue" num={3} />
-    </div>
+    <>
+      <div>
+        <Button onClick={handleRollClick}>던지기</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
+      </div>
+      <Dice color="blue" num={num} />
+    </>
   );
 }
 
